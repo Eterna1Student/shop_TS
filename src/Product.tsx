2f000341006b9babea5c450b990664f565e5914a
Oleg <oleg-kozlov91@rambler.ru>
2024-12-25 23:02:16 +0300

    add cart

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,62 +1,74 @@
 import axios from "axios";
-import {Link} from "react-router";
 import { useEffect, useState } from "react";
-import type {ProductType} from "./BestSalers.tsx";
+import { Link, useParams } from "react-router";
+import type { Product as ProductType } from "./BestSalers.tsx";
 import rating from "./assets/img/rating.svg";
 import cartWhite from "./assets/img/cartWhite.svg";
-import {useParams} from "react-router";
+import cart from "./assets/img/cart.svg";
 import arrowBack from "./assets/img/arrowBack.svg";
-import {Reviews} from "./Reviews.tsx";
+import { Reviews } from "./Reviews.tsx";
 
 export const Product = () => {
     const [product, setProduct] = useState<ProductType | null>(null);
 
-    const { productId } = useParams();
+    const [isProductInCart, setIsProductIncart] = useState<boolean>(false);
 
+    const { productId } = useParams();
 
     useEffect(() => {
         axios
-            .get(`https://masterclass.kimitsu.it-incubator.io/api/products/${productId}`)
-            .then((res) => {
-                const product = res.data;
-                setProduct(product);
-            })
-    }, [])
+            .get(
+                `https://masterclass.kimitsu.it-incubator.io/api/products/${productId}`,
+            )
+            .then((res) => setProduct(res.data));
+    }, []);
+
+    const addProductToCartHandler = () => {
+        alert("Товар успешно добавлен в корзину");
+        setIsProductIncart(true);
+    };
 
     if (product === null) {
         return <h2>Продукт еще грузится ...</h2>;
     }
 
     return (
-        <div>
+        <>
             <div className="arrowBack">
-                <Link to={'/'}>
-                    <img src={arrowBack} alt="arrowBack"/>
+                <Link to={"/"}>
+                    <img src={ arrowBack } alt="arrowBack" />
                     Back to Best Seller
                 </Link>
             </div>
 
             <div className="product">
-                <img src={product.image} alt=""/>
+                <img src={product.image} alt="" />
                 <div className="info">
                     <p className="title">{product.title}</p>
                     <p className="price">$ {product.price}</p>
                     <div className="rating">
                         <p>Rating: {product.rating.rate}</p>
-                        <img src={rating} alt=""/>
+                        <img src={rating} alt="" />
                     </div>
                     <div className="category">
                         <span>Category:</span>
                         <p>{product.category}</p>
                     </div>
                     <p className="description">{product.description}</p>
-                    <button>
-                        <img src={cartWhite} alt=""/>
-                        Add to cart
+
+                    <button
+                        onClick={addProductToCartHandler}
+                        className={`button ${isProductInCart ? "active" : ""}`}
+                    >
+                        <img src={isProductInCart ? cart : cartWhite} alt="cart icon" />
+                        {isProductInCart ? "Go to cart" : "Add to cart"}
                     </button>
                 </div>
             </div>
+
             <Reviews />
-        </div>
-    )
-}
+        </>
+    );
+};
+
+ 
